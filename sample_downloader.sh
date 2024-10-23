a0a76df72e4726772eb45f650842045ff81f6a7a
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2024-10-24 00:03:02 +0300

    Update sample_downloader.sh

--- sample_downloader.sh
+++ sample_downloader.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Sample downloader script
 # Creating a variable with the accession number of the sample
+mkdir -p samples
+dir="samples"
 sample=$(cat $1)
 for i in $sample
 do
 	echo "Downloading $i"
-	fasterq-dump $i
+	fasterq-dump $i -o $dir
 	echo "Done downloading $i"
 done
