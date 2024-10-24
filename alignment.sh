5abe6c184fae9edd5146ca1d52a7da2267f20241
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2024-10-24 18:23:05 +0300

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -6,13 +6,14 @@ sample=$(cat samples.txt)
 ref="reference-NC_045512.fasta"
 for i in $sample
 do
-  R1=${i}_trimmed_R1.fq.gz
-  R2=${i}_trimmed_R2.fq.gz
+  R1=${i}_trimmed_R1.fq
+  R2=${i}_trimmed_R2.fq
   echo "Performing alignment of $i"
-	bwa mem $ref $R1 $R2 > $dir/${i}.sam
+	bwa mem $ref samples/$R1 samples/$R2 > $dir/${i}.sam
 	samtools view -O BAM -o $dir/${i}.bam $dir/${i}.sam
 	samtools sort $dir/${i}.bam > $dir/${i}_sorted.bam
 	samtools index $dir/${i}_sorted.bam
-	#freebayes -f $2 $dir/${n}_sorted.bam > $dir/${n}.vcf
+	#freebayes -f $2 $dir/${i}_sorted.bam > $dir/${i}.vcf
 done
 
+#samples/${i}_trimmed_R1.fq
