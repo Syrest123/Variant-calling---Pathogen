cb7543ffa0fd9ca55a456e916784114ea1fe11d8
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2024-10-24 19:53:56 +0300

    Update variant_caller.sh

--- variant_caller.sh
+++ variant_caller.sh
@@ -8,6 +8,5 @@ ref="seq.fasta"
 for i in $sample
 do
   echo "Performing variant calling of $i"
-  	samtools faidx $dir/${i}_sorted.bam
 	freebayes -f ref/$ref $dir/${i}_sorted.bam > $dir1/${i}.vcf
 done
