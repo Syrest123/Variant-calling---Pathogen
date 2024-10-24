12ab1203fa6949bc971ba9e687f8f9d0ca40cffa
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2024-10-24 19:49:09 +0300

    Update variant_caller.sh

--- variant_caller.sh
+++ variant_caller.sh
@@ -4,9 +4,9 @@ mkdir -p vcf
 dir="alignment"
 dir1="vcf"
 sample=$(cat samples.txt)
-ref="reference-NC_045512.fasta"
+ref="seq.fasta"
 for i in $sample
 do
   echo "Performing variant calling of $i"
-	freebayes -f $ref $dir/${i}_sorted.bam > $dir1/${i}.vcf
+	freebayes -f ref/$ref $dir/${i}_sorted.bam > $dir1/${i}.vcf
 done
