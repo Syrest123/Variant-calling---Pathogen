e10a85f95f899b29da99a9814c90e6275779492a
Semawule Syrus <137260746+Syrest123@users.noreply.github.com>
2024-10-24 11:27:54 +0300

    Update sample_downloader.sh

--- sample_downloader.sh
+++ sample_downloader.sh
@@ -7,6 +7,7 @@ sample=$(cat $1)
 for i in $sample
 do
 	echo "Downloading $i"
-	fasterq-dump $i -O $dir
+	#fasterq-dump $i -O $dir
+ 	fastq-dump $i --split-files -O $dir -N 10000 -x 110000
 	echo "Done downloading $i"
 done
